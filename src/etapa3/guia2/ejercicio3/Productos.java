eff42861bab904a8c569a5edb4f26466d073f023
CharlyCorvalan <charlygohan@gmail.com>
2023-08-14 16:56:06 -0300

    Initial commit

---
+++ src/etapa3/guia2/ejercicio3/Productos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etapa3.guia2.ejercicio3;
+
+/**
+ *
+ * @author charl
+ */
+public class Productos {
+    private String nombre, categoria;
+    private int precio;
+
+    public Productos(String nombre, String categoria, int precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+}
